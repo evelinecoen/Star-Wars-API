626bc72e2af689b42915b59181731b8700f72234
“Eveline <eveline-c@hotmail.com>
2023-03-29 17:52:00 +0100

    styles homepage and navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import PlanetsAPI from './PlanetsAPI'
 import NewPlanets from './NewPlanets'
 import AddPlanet from './AddPlanet'
+import { Link } from 'react-router-dom'
+import '../styles/Home.css'
 
 
 function Home() {
+  
   return (
-    <div>Home</div>
+    <div className='home'>
+        
+        <div className='titles'>
+            <h1>Fancy title</h1>
+            <h2>very cool subtitle</h2>
+        </div>
+        <div className='home-page-buttons'>
+            <button><Link className='button-text' to='/planets'>Planets</Link></button>
+            <button><Link className='button-text' to='/newplanets'>New Planets</Link></button>
+            <button><Link className='button-text' to='/addplanet'>Add Planet</Link></button>
+        </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
 
 export default function Navbar() {
   return (
-    <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/planets'>Planets</Link></li>
-        <li><Link to='/newplanets'>New Planets</Link></li>
-        <li><Link to='/addplanet'>Add Planet</Link></li>
+    <ul className='navbar'>
+        <li><Link className='navbar-text' to='/'>Home</Link></li>
+        <li><Link className='navbar-text' to='/planets'>Planets</Link></li>
+        <li><Link className='navbar-text' to='/newplanets'>New Planets</Link></li>
+        <li><Link className='navbar-text' to='/addplanet'>Add Planet</Link></li>
 
     </ul>
   )
